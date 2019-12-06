8a0466780c7d76f3efa91ce39fd60221cef6e5a7
Gent <lordgent@gmail.com>
2019-12-06 12:01:04 +0700

    Update cni.py

--- cni.py
+++ cni.py
@@ -2,24 +2,24 @@
 from PIL import Image
 
 icons = {
-	"Icon.png": 57,
-	"Icon-ldpi.png": 36,
-	"Icon-mdpi.png": 48,
-	"Icon-hdpi.png": 72,
-	"Icon-xhdpi.png": 96,
-	"Icon-xxhdpi.png": 144,
-	"Icon-xxxhdpi.png": 192,
+	"android/Icon.png": 57,
+	"android/Icon-ldpi.png": 36,
+	"android/Icon-mdpi.png": 48,
+	"android/Icon-hdpi.png": 72,
+	"android/Icon-xhdpi.png": 96,
+	"android/Icon-xxhdpi.png": 144,
+	"android/Icon-xxxhdpi.png": 192,
 
-	"Icon-40.png": 40,
-	"Icon-58.png": 58,
-	"Icon-76.png": 76,
-	"Icon-80.png": 80,
-	"Icon-87.png": 87,
-	"Icon-120.png": 120,
-	"Icon-152.png": 152,
-	"Icon-167.png": 167,
-	"Icon-180.png": 180,
-	"Icon-1024.png": 1024,
+	"ios/Icon-40.png": 40,
+	"ios/Icon-58.png": 58,
+	"ios/Icon-76.png": 76,
+	"ios/Icon-80.png": 80,
+	"ios/Icon-87.png": 87,
+	"ios/Icon-120.png": 120,
+	"ios/Icon-152.png": 152,
+	"ios/Icon-167.png": 167,
+	"ios/Icon-180.png": 180,
+	"ios/Icon-1024.png": 1024,
 
 	"IconNotificationDefault-ldpi.png": 16,
 	"IconNotificationDefault-mdpi.png": 24,
@@ -32,6 +32,6 @@ icons = {
 
 for name in icons:
 	im = Image.open( "upload.png" )
-	im.thumbnail((icons[name], icons[name]), Image.ANTIALIAS)
+	im.resize((icons[name], icons[name]), Image.ANTIALIAS)
 	im.save(name, "PNG")
 	im.close()
